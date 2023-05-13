ce0ad08262cb7bd475af326bc955efae11b0d319
mykolachupyak <chupyak@gmail.com>
2023-05-13 16:28:18 +0300

    Initial commit

---
+++ src/main/java/org/ukrposhtarest/UkrposhtaRestApplication.java
@@ -0,0 +1,13 @@
+package org.ukrposhtarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkrposhtaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkrposhtaRestApplication.class, args);
+	}
+
+}
